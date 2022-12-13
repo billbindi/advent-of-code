9fab0ec2ca2f9e9f224c9c6a27245d502ba8543a
Bill Bindi <billdbindi@gmail.com>
2022-12-13 12:02:23 -0800

    [2017] Day 5

---
+++ 2017/src/main/java/advent2017/Day5_1.java
@@ -0,0 +1,35 @@
+package advent2017;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day5_1 {
+
+    private static final String FILENAME = "2017/day5_input.txt";
+
+    public static void main(String[] args) throws IOException {
+        int[] instructions = parseInstructions(Files.readAllLines(Paths.get(FILENAME)));
+        int count = countSteps(instructions);
+        System.out.println(count);
+    }
+
+    private static int[] parseInstructions(List<String> lines) {
+        return lines.stream()
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static int countSteps(int[] instructions) {
+        int index = 0;
+        int steps = 0;
+        while (index >= 0 && index < instructions.length) {
+            int jump = instructions[index];
+            instructions[index]++;
+            index += jump;
+            steps++;
+        }
+        return steps;
+    }
+}
