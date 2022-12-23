8fc379526d649138eeee67343fc385db57637ab2
Bill Bindi <billdbindi@gmail.com>
2022-12-22 21:35:05 -0800

    [2018] Day 2

---
+++ 2018/src/main/java/advent2018/Day2_1.java
@@ -0,0 +1,49 @@
+package advent2018;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day2_1 {
+
+    private final static String FILENAME = "2018/day2_input.txt";
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(FILENAME));
+        System.out.println(solve(lines));
+    }
+
+    private static String solve(List<String> lines) {
+        int twoCount = 0;
+        int threeCount = 0;
+        for (String line : lines) {
+            Map<Character, Integer> letterCounts = new HashMap<>();
+            for (char character : line.toCharArray()) {
+                if (!letterCounts.containsKey(character)) {
+                    letterCounts.put(character, 0);
+                }
+                int newCount = letterCounts.get(character) + 1;
+                letterCounts.put(character, newCount);
+            }
+            if (hasCount(letterCounts, 2)) {
+                twoCount++;
+            }
+            if (hasCount(letterCounts, 3)) {
+                threeCount++;
+            }
+        }
+        return Integer.toString(twoCount * threeCount);
+    }
+
+    private static boolean hasCount(Map<Character, Integer> letterCounts, int expected) {
+        for (Integer count : letterCounts.values()) {
+            if (count == expected) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ 2018/src/main/java/advent2018/Day2_2.java
@@ -0,0 +1,58 @@
+package advent2018;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Day2_2 {
+
+    private final static String FILENAME = "2018/day2_input.txt";
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(FILENAME));
+        System.out.println(solve(lines));
+    }
+
+    // Brute force is more than efficient enough for small input.
+    // Slight optimization in only checking further in the list,
+    // no need to chack (a, b) and (b, a) pairs.
+    private static String solve(List<String> lines) {
+        for (int i = 0; i < lines.size(); i++) {
+            for (int j = i + 1; j < lines.size(); j++) {
+                String first = lines.get(i);
+                String second = lines.get(j);
+                String check = checkMatch(first, second);
+                if (check != null) {
+                    return check;
+                }
+            }
+        }
+        throw new IllegalStateException("NO MATCHES FOUND");
+    }
+
+    @Nullable
+    private static String checkMatch(String first, String second) {
+        // just making sure
+        if (first.length() != second.length()) {
+            return null;
+        }
+        boolean mismatched = false;
+        int mismatchIndex = 0;
+        for (int i = 0; i < first.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                if (mismatched) {
+                    // second mismatch, no good
+                    return null;
+                } else {
+                    mismatched = true;
+                    mismatchIndex = i;
+                }
+            }
+        }
+
+        // only one mismatch, return rest of string
+        return first.substring(0, mismatchIndex) + first.substring(mismatchIndex + 1);
+    }
+}
