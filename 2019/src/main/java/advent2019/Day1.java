8999f88c1ce2e5f0c262f46b6f27d1419f555e64
Bill Bindi <billdbindi@gmail.com>
2025-12-22 19:49:08 -0800

    Day 1 part 1

--- 2019/src/main/java/advent2019/Day1.java
+++ 2019/src/main/java/advent2019/Day1.java
@@ -1,7 +1,27 @@
 package advent2019;
 
+import com.google.common.base.Preconditions;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class Day1 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    private static final String INPUT_FILENAME = "2019/day1_input.txt";
+    private static final Path INPUT_PATH = Path.of(INPUT_FILENAME);
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(part1(Files.lines(INPUT_PATH)));
+        // System.out.println(part2(Files.lines(INPUT_PATH)));
+    }
+
+    private static int part1(Stream<String> lines) {
+        return lines.mapToInt(Integer::parseInt).map(Day1::fuelNeeded).sum();
+    }
+
+    private static int fuelNeeded(int mass) {
+        Preconditions.checkArgument(mass > 8, "Will have negative fuel needed!");
+        return (mass / 3) - 2;
     }
 }
