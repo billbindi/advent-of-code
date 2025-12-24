774b2685f69e9ad2a7cb8227bc92a23042615dab
Bill Bindi <billdbindi@gmail.com>
2025-12-23 19:49:59 -0800

    Day 3 part 1

--- 2019/src/main/java/advent2019/Day3.java
+++ 2019/src/main/java/advent2019/Day3.java
@@ -1,8 +1,15 @@
 package advent2019;
 
+import com.google.common.base.Splitter;
+import util.Coordinate;
+import util.Direction;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public final class Day3 {
@@ -15,10 +22,114 @@ public final class Day3 {
     }
 
     private static int part1(Stream<String> lines) {
-        return 0;
+        // build list of wires
+        List<Wire> wires = lines.map(Day3::buildWire).toList();
+
+        // find intersection point. arbitrary stop point in search, could
+        // be replaced by a tracker of seeing at least one part of every
+        // wire at this layer (no intersection possible past that point),
+        // but that extra overhead is not wort the effort.
+        for (int manhattanDistance = 1; manhattanDistance < 10000; manhattanDistance++) {
+            // make a diamond centered on the origin. start at the far right and sweep around CCW.
+            // will double test all endpoints, that is fine.
+
+            // (m, 0) -> (0, m)
+            for (int x = manhattanDistance, y = 0; x >= 0; x--, y++) {
+                Coordinate test = new Coordinate(x, y);
+                if (wires.stream().allMatch(wire -> wire.contains(test))) {
+                    return manhattanDistance;
+                }
+            }
+
+            // (0, m) -> (-m, 0)
+            for (int x = 0, y = manhattanDistance; y >= 0; x--, y--) {
+                Coordinate test = new Coordinate(x, y);
+                if (wires.stream().allMatch(wire -> wire.contains(test))) {
+                    return manhattanDistance;
+                }
+            }
+
+            // (-m, 0) -> (0, -m)
+            for (int x = -manhattanDistance, y = 0; x <= 0; x++, y--) {
+                Coordinate test = new Coordinate(x, y);
+                if (wires.stream().allMatch(wire -> wire.contains(test))) {
+                    return manhattanDistance;
+                }
+            }
+
+            // (0, -m) -> (m, 0)
+            for (int x = 0, y = -manhattanDistance; y <= 0; x++, y++) {
+                Coordinate test = new Coordinate(x, y);
+                if (wires.stream().allMatch(wire -> wire.contains(test))) {
+                    return manhattanDistance;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static Wire buildWire(String line) {
+        Wire wire = new Wire();
+        Coordinate tracker = new Coordinate(0, 0);
+        Iterable<String> paths = Splitter.on(',').split(line);
+        for(String path : paths) {
+            int length =  Integer.parseInt(path.substring(1));
+            tracker = switch (path.charAt(0)) {
+                case 'L' -> {
+                    wire.addAll(tracker, Direction.LEFT, length);
+                    yield new Coordinate(tracker.getX() - length, tracker.getY());
+                }
+                case 'R' -> {
+                    wire.addAll(tracker, Direction.RIGHT, length);
+                    yield new Coordinate(tracker.getX() + length, tracker.getY());
+                }
+                case 'U' -> {
+                    wire.addAll(tracker, Direction.UP, length);
+                    yield new Coordinate(tracker.getX(), tracker.getY() + length);
+                }
+                case 'D' -> {
+                    wire.addAll(tracker, Direction.DOWN, length);
+                    yield new Coordinate(tracker.getX(), tracker.getY() - length);
+                }
+                default -> throw new IllegalArgumentException("Invalid input: " + path);
+            };
+        }
+        return wire;
     }
 
     private static int part2(Stream<String> lines) {
         return 0;
+    }
+
+    private static final class Wire {
+        private final Set<Coordinate> coordinates = new HashSet<>();
+
+        public void addAll(Coordinate start, Direction direction, int amount) {
+            int addX = switch (direction) {
+                case LEFT -> -1;
+                case RIGHT -> 1;
+                case UP, DOWN -> 0;
+            };
+            int addY = switch (direction) {
+                case LEFT, RIGHT -> 0;
+                case UP -> 1;
+                case DOWN -> -1;
+            };
+
+            // include start, though not strictly speaking necessary
+            for (int count = 0; count <= amount; count++) {
+                int x = start.getX() + (count * addX);
+                int y = start.getY() + (count * addY);
+                coordinates.add(new Coordinate(x, y));
+            }
+        }
+
+        public boolean contains(Coordinate coordinate) {
+            return coordinates.contains(coordinate);
+        }
+
+        public boolean contains(int x, int y) {
+            return contains(new Coordinate(x, y));
+        }
     }
 }
